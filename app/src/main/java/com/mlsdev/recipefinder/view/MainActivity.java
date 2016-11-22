7de77d8ca200014c707ac066312f91f83d5991cf
sergeypetrosyuk <sergeypetrosyuk@gmail.com>
2016-11-22 18:16:39 +0200

    Added navigation items and updated the project structure

--- app/src/main/java/com/mlsdev/recipefinder/view/MainActivity.java
+++ app/src/main/java/com/mlsdev/recipefinder/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.mlsdev.recipefinder;
+package com.mlsdev.recipefinder.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.mlsdev.recipefinder.R;
 
 public class MainActivity extends AppCompatActivity {
 
