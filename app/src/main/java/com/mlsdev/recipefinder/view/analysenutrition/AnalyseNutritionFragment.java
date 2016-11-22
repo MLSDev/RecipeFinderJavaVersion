de575b4ce2bf5117d34a730d70448a51a6caf943
sergeypetrosyuk <sergeypetrosyuk@gmail.com>
2016-11-22 19:37:54 +0200

    Added basic fragment\'s implementation

---
+++ app/src/main/java/com/mlsdev/recipefinder/view/analysenutrition/AnalyseNutritionFragment.java
@@ -0,0 +1,26 @@
+package com.mlsdev.recipefinder.view.analysenutrition;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mlsdev.recipefinder.R;
+import com.mlsdev.recipefinder.databinding.FragmentAnalyseNutritionBinding;
+
+public class AnalyseNutritionFragment extends Fragment {
+    private FragmentAnalyseNutritionBinding binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search_recipes, container, false);
+
+        return binding.getRoot();
+    }
+}
