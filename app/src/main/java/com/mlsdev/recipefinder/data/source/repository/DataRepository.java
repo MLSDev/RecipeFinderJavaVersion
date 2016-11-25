c1b4607ebaf23f6c80dd9e1aab6ce9f2db6c369a
Serhii Petrosyuk <petrosyuk@mlsdev.com>
2016-11-25 15:59:54 +0200

    Added the repository

--- app/src/main/java/com/mlsdev/recipefinder/data/source/repository/DataRepository.java
+++ app/src/main/java/com/mlsdev/recipefinder/data/source/repository/DataRepository.java
@@ -1,33 +1,101 @@
 package com.mlsdev.recipefinder.data.source.repository;
 
 import android.content.Context;
+import android.support.v4.util.ArrayMap;
 
+import com.mlsdev.recipefinder.data.entity.Hit;
+import com.mlsdev.recipefinder.data.entity.Recipe;
 import com.mlsdev.recipefinder.data.entity.SearchResult;
 import com.mlsdev.recipefinder.data.source.DataSource;
 import com.mlsdev.recipefinder.data.source.remote.ParameterKeys;
 import com.mlsdev.recipefinder.data.source.remote.RemoteDataSource;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import rx.Observable;
-import rx.functions.Action1;
+import rx.functions.Action0;
+import rx.functions.Func1;
 
-public class DataRepository extends PaginatedDataSource implements DataSource {
+public class DataRepository {
+    private final int offset = 10;
+    private int from = 0;
+    private int to = offset;
+    private boolean more = true;
+
+    private static DataRepository instance;
     private DataSource remoteDataSource;
 
-    public DataRepository(Context context) {
+    private Map<String, Recipe> cachedRecipes;
+    private boolean cacheIsDirty = false;
+
+    private DataRepository(Context context) {
         remoteDataSource = new RemoteDataSource(context);
+        cachedRecipes = new ArrayMap<>();
     }
 
-    @Override
-    public Observable<SearchResult> searchRecipes(Map<String, String> params) {
-        params.put(ParameterKeys.FROM, String.valueOf(0));
-        params.put(ParameterKeys.TO, String.valueOf(20));
-        return remoteDataSource.searchRecipes(params).doOnNext(new Action1<SearchResult>() {
-            @Override
-            public void call(SearchResult searchResult) {
-                DataRepository.this.setupPagination(searchResult.getTo(), searchResult.isMore());
-            }
-        });
+    public static DataRepository getInstance(Context context) {
+        if (instance == null)
+            instance = new DataRepository(context);
+
+        return instance;
     }
+
+    public void setCacheIsDirty() {
+        cacheIsDirty = true;
+    }
+
+    public Observable<List<Recipe>> searchRecipes(Map<String, String> params) {
+        if (!cacheIsDirty) {
+            return Observable.from(cachedRecipes.values()).toList();
+        }
+
+        cachedRecipes.clear();
+
+        params.put(ParameterKeys.FROM, String.valueOf(0));
+        params.put(ParameterKeys.TO, String.valueOf(offset));
+
+        return getRecipes(params);
+    }
+
+    public Observable<List<Recipe>> loadMore(Map<String, String> params) {
+
+        if (!more) {
+            return Observable.from(new ArrayList<Recipe>()).toList();
+        }
+
+        params.put(ParameterKeys.FROM, String.valueOf(from));
+        params.put(ParameterKeys.TO, String.valueOf(to));
+
+        return getRecipes(params);
+    }
+
+    private Observable<List<Recipe>> getRecipes(Map<String, String> params) {
+        return remoteDataSource.searchRecipes(params)
+                .map(new Func1<SearchResult, List<Recipe>>() {
+                    @Override
+                    public List<Recipe> call(SearchResult searchResult) {
+                        from = searchResult.getTo();
+                        to = from + offset;
+                        more = searchResult.isMore();
+
+                        Map<String, Recipe> recipes = new ArrayMap<>();
+
+                        for (Hit hit : searchResult.getHits())
+                            recipes.put(hit.getRecipe().getUri(), hit.getRecipe());
+
+                        cachedRecipes.putAll(recipes);
+
+                        return (List<Recipe>) recipes.values();
+                    }
+                })
+                .doOnCompleted(new Action0() {
+                    @Override
+                    public void call() {
+                        cacheIsDirty = false;
+                    }
+                });
+    }
+
 }
