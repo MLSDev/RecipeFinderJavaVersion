6e8d367ea12892b174ad0d9b27ac568ed96a9ef4
sergeypetrosyuk <sergeypetrosyuk@gmail.com>
2016-12-06 21:35:45 +0200

    Added an empty view for the favorites list

--- app/src/main/java/com/mlsdev/recipefinder/view/favoriterecipes/FavoritesViewModel.java
+++ app/src/main/java/com/mlsdev/recipefinder/view/favoriterecipes/FavoritesViewModel.java
@@ -1,6 +1,8 @@
 package com.mlsdev.recipefinder.view.favoriterecipes;
 
 import android.content.Context;
+import android.databinding.ObservableInt;
+import android.view.View;
 
 import com.mlsdev.recipefinder.data.entity.Recipe;
 import com.mlsdev.recipefinder.view.listener.OnRecipesLoadedListener;
@@ -10,14 +12,17 @@ import java.util.List;
 
 public class FavoritesViewModel extends BaseViewModel {
     private OnRecipesLoadedListener onRecipesLoadedListener;
+    public final ObservableInt emptyViewVisibility;
 
     public FavoritesViewModel(Context context, OnRecipesLoadedListener onRecipesLoadedListener) {
         super(context);
         this.onRecipesLoadedListener = onRecipesLoadedListener;
+        emptyViewVisibility = new ObservableInt(View.VISIBLE);
     }
 
     public void getFavoriteRecipes() {
         List<Recipe> recipes = repository.getFavoriteRecipes();
+        emptyViewVisibility.set(recipes.isEmpty() ? View.VISIBLE : View.INVISIBLE);
         onRecipesLoadedListener.onRecipesLoaded(recipes);
     }
 }
