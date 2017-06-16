97868cf21d2ad5bef00197b95570f97846be52dd
Serhii Petrosyuk <petrosyuk@mlsdev.com>
2017-06-16 17:35:44 +0300

    Added the spoon integration

--- app/src/androidTest/java/com/mlsdev/recipefinder/SearchRecipesFragmentTest.java
+++ app/src/androidTest/java/com/mlsdev/recipefinder/SearchRecipesFragmentTest.java
@@ -8,6 +8,7 @@ import android.support.test.rule.ActivityTestRule;
 
 import com.mlsdev.recipefinder.data.entity.recipe.Recipe;
 import com.mlsdev.recipefinder.view.MainActivity;
+import com.squareup.spoon.Spoon;
 
 import org.junit.After;
 import org.junit.Before;
@@ -48,16 +49,27 @@ public class SearchRecipesFragmentTest extends BaseTest {
     public void testStartSearching() {
         mockWebServer.enqueue(new MockResponse().setBody(AssetUtils.getSearchResultJsonData(getContext())));
         mockWebServer.enqueue(new MockResponse().setBody(AssetUtils.getMoreRecipesJsonData(getContext())));
+
+        Spoon.screenshot(rule.getActivity(), "Search");
+
         performSearch();
 
         onView(withText(firstRecipeTitle)).check(matches(isDisplayed()));
         onView(withId(R.id.rv_recipe_list)).perform(RecyclerViewActions.scrollToPosition(9));
         onView(withText(lastRecipeTitle)).check(matches(isDisplayed()));
 
+        Spoon.screenshot(rule.getActivity(), "Search");
+
         // Scroll to top and click upon the first item
         Recipe recipe = AssetUtils.getRecipeEntity(InstrumentationRegistry.getContext());
         onView(withId(R.id.rv_recipe_list)).perform(RecyclerViewActions.scrollToPosition(0));
+
+        Spoon.screenshot(rule.getActivity(), "Search");
+
         onView(withText(recipe.getLabel())).perform(ViewActions.click());
+
+        Spoon.screenshot(rule.getActivity(), "Search");
+
         onView(withId(R.id.tv_health_labels)).check(matches(isDisplayed()));
         onView(withId(R.id.iv_recipe_image)).check(matches(isDisplayed()));
         onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
@@ -69,12 +81,14 @@ public class SearchRecipesFragmentTest extends BaseTest {
 
     private void performSearch() {
         onView(withId(R.id.action_search)).perform(ViewActions.click());
+        Spoon.screenshot(rule.getActivity(), "Search_Open_search_field");
 
-        onView(withId(R.id.ed_search_text))
-                .perform(ViewActions.typeText(searchText))
-                .perform(ViewActions.pressImeActionButton());
+        onView(withId(R.id.ed_search_text)).perform(ViewActions.typeText(searchText));
+        Spoon.screenshot(rule.getActivity(), "Search_Enter_the_search_phrase");
 
+        onView(withId(R.id.ed_search_text)).perform(ViewActions.pressImeActionButton());
         closeSoftKeyboard();
+        Spoon.screenshot(rule.getActivity(), "Search_Start_search");
     }
 
     @Test
@@ -82,6 +96,7 @@ public class SearchRecipesFragmentTest extends BaseTest {
         mockWebServer.enqueue(new MockResponse().setBody(AssetUtils.getEmptySearchResultJsonData(getContext())));
         performSearch();
         onView(withText(R.string.label_search_nothing_found)).check(matches(isDisplayed()));
+        Spoon.screenshot(rule.getActivity(), "Search_Nothing_found");
     }
 
     @Test
@@ -89,6 +104,7 @@ public class SearchRecipesFragmentTest extends BaseTest {
         mockWebServer.enqueue(new MockResponse().setResponseCode(500));
         performSearch();
         onView(withText(R.string.error_message_technical)).check(matches(isDisplayed()));
+        Spoon.screenshot(rule.getActivity(), "Search_Technical_error");
     }
 
     @Test
@@ -96,5 +112,6 @@ public class SearchRecipesFragmentTest extends BaseTest {
         mockWebServer.enqueue(new MockResponse().throttleBody(1024, 1, TimeUnit.SECONDS));
         performSearch();
         onView(withText(R.string.error_message_common)).check(matches(isDisplayed()));
+        Spoon.screenshot(rule.getActivity(), "Search_Common_error");
     }
 }
